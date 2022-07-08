c574ba98bd943c0aa644121fd8b4ec8114873750
Morgane Julian <morganne.j@live.fr>
2022-07-08 10:05:09 +0200

    Initial Commit

---
+++ WeatherAPI/WeatherAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPIApp.swift
+//  WeatherAPI
+//
+//  Created by Morgane Julian on 08/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
